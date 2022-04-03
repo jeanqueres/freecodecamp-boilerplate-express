8bf7203374f5671cc854e5112a04e3fb3da89c4e
jeanqueres <jean.souzaqueres@gmail.com>
2022-04-03 00:06:13 -0300

    hello express

--- myApp.js
+++ myApp.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 })
 
-app.use(__dirname + '/public', express.static());
+app.use('/public', express.static(__dirname + '/public'));
 
 module.exports = app;
