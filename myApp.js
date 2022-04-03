05f971ee5dab5ce3a06f9cd131bc3c37c4640111
jeanqueres <jean.souzaqueres@gmail.com>
2022-04-02 23:57:16 -0300

    hello express

--- myApp.js
+++ myApp.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+/* 
+    console.log("Hello World");
+*/
+
+/*
 app.get('/', (req, res) => {
     res.send('Hello Express');
 })
+*/
 
-console.log("Hello World");
+
+app.get('/', (req, res) => {
+    const absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath);
+})
 
 module.exports = app;
